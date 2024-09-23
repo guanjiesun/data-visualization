91008ddc0ae419e23e97ab4ce500e09d9345992a
Guanjie <gjsun@666.com>
2024-09-23 21:18:54 +0800

    plot a figure

---
+++ mpl_squares.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def main():
+    squares = [1, 4, 9, 16, 25]
+    fig, ax = plt.subplots()
+    ax.plot(squares)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
